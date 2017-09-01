2843f2502f119bbda3779848159e710d7687c41a
Sujit Joshi <joshi.sujit020386@gmail.com>
2017-09-01 11:19:25 -0400

    This is my initial commit to play around with git

---
+++ app/src/androidTest/java/com/hackerrank/joshi/hackerrank/DeleteMe.java
@@ -0,0 +1,11 @@
+package com.hackerrank.joshi.hackerrank;
+
+/**
+ * Created by joshi on 9/10/2016.
+ */
+public class DeleteMe {
+
+    public static void main(String[] lkjhadf){
+        System.out.println("Sujit");
+    }
+}
